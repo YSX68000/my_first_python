95277663ce2b80e0f543c18cc2c4491f3ba9dde2
YSX68000 <157483209+YSX68000@users.noreply.github.com>
2024-01-23 13:49:25 +0200

    Add files via upload

---
+++ my_first_python.py
@@ -0,0 +1,9 @@
+#My first python code
+#Japanese name generator
+print("Type your first name here:")
+x = input()
+if "o" in x:
+    print(x + " " + "should be" + " " + "Yoshiki")
+else:
+    print(x + " " + "should be" + " " + "Hide")
+    
